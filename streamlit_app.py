e28dd9979925e2764a9af4f952e89db7d1349a61
Christian Flasshoff <54878764+chrfla@users.noreply.github.com>
2021-06-03 21:19:58 +0200

    add page title

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ import altair as alt
 from urllib.error import URLError
 
 
+st.set_page_config(page_title="Quarantine Regulations - Norway ", page_icon="🇳🇴")
+
 st.title("Current COVID-19 Statistics Europe")
 
 
